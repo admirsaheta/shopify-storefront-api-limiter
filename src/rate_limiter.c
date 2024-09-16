04183e2145eb4837e2313019c15a083911ecad22
admirsaheta <admirxsaheta@gmail.com>
2024-09-16 22:45:08 +0200

    basics:protocol

---
+++ src/rate_limiter.c
@@ -0,0 +1,19 @@
+#ifndef RATE_LIMITER_H
+#define RATE_LIMITER_H
+
+#include <curl/curl.h>
+
+// Since I am testing around protocol-oriented programming in C, here's a protocol implementation
+// Most of the time, I would use a interface in C++ 
+typedef struct RateLimiter {
+    void (*limit)(const char* url);
+    void (*handle_rate_limit)(long remaining_requests);
+} RateLimiter;
+
+// We need to initialize the rate limiter because C doesn't have constructors
+RateLimiter* new_rate_limiter(void);
+
+// Release the rate limiter because C doesn't have destructors
+void free_rate_limiter(RateLimiter* limiter);
+
+#endif
